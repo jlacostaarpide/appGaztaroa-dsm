00e1e1d7f2403c7b9679d769a8dd7518e2aa5b77
Juan Lacosta <126659696+jlacostaarp-unav@users.noreply.github.com>
2026-03-29 21:31:27 +0200

    Stack Navigation

--- App.js
+++ App.js
@@ -16,13 +16,4 @@ export default function App() {
       </PaperProvider>
     </SafeAreaProvider>
   );
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
 }
