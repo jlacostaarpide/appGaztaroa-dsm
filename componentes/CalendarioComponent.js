5839203c5d939cb27d5937615696a32154374f54
Juan Lacosta <126659696+jlacostaarp-unav@users.noreply.github.com>
2026-03-29 21:21:50 +0200

    Componentes funcionales en React Native

--- componentes/CalendarioComponent.js
+++ componentes/CalendarioComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FlatList, View, Image, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { List, Divider } from 'react-native-paper';
-function Calendario({ excursiones }) {
+function Calendario({ excursiones, onPress }) {
     const renderCalendarioItem = ({ item }) => {
         return (
             <View>
@@ -11,6 +11,7 @@ function Calendario({ excursiones }) {
                     description={item.descripcion}
                     titleNumberOfLines={0}
                     descriptionNumberOfLines={6}
+                    onPress={() => onPress(item.id)}
                     left={(props) => (
                         <Image
                             source={require('./imagenes/40Años.png')}
